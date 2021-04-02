a0e9dab3ebbcce42caf605eb445f9b2071efbf3a
Shubham <25881429+shoebham@users.noreply.github.com>
2021-04-02 23:12:45 +0530

    changed some things

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -1,34 +1,82 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+
+class bubble_sort 
+{
+	public:
+	vector<int> arr;
+	int size,total=0;
+	
+	bubble_sort()
+	{
+		input_arr();
+		sort();
+		display();
+	}
+	
+	void input_arr()
+	{
+		cout<<"Enter size of array\n";
+		cin>>size;
+		cout<<"Enter elements\n";
+		for(int i=0;i<size;i++)
+		{
+			int n;
+			cin>>n;
+			arr.push_back(n);
+			
+		}
+	}
+	
+	void sort()
+	{
+		
+		cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+		for(int i=0;i<size;i++)
+		{
+//			cout<<"Pass "<<i<<":\t|\n";
+			for(int j=0;j<size-1;j++)
+			{
+				cout<<"i = "<<i<<" --> ";
+				
+				for(int k=0;k<size;k++)
+				{
+					cout<<"|"<<arr[k]<<"|";
+				}	cout<<" <-- "<<j<<" = j ";
+				
+				if(arr[j]<arr[j+1])
+				{
+					cout<<"("<<arr[j+1]<<" > "<<arr[j]<<") No swap \t|";
+				}
+				
+				if(arr[j]>arr[j+1])
+				{
+					cout<<"("<<arr[j]<<" > "<<arr[j+1]<<") swap \t|";
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					total++;
+				}
+				cout<<endl;
+			}
+		cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";	
+		}
+	}
+	void display()
+	{
+		for(int i=0;i<size;i++)
+		{
+			cout<<"|"<<arr[i]<<"|";
+		}
+		cout<<"\nTotal passes: "<<size-1<<" Total comparisions: "<<size*(size-1)<<" Total swaps: "<<total<<endl;
+	}
+};
 int main()
 {
-	int arr[] = {5,4,3,2,1};
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4;j++)
-		{
-			cout<<"i = "<<i<<" --> ";
-			for(int k=0;k<5;k++)
-			{
-				cout<<"|"<<arr[k]<<"|";
-			}	cout<<" <-- "<<j<<" = j ";
-			if(arr[j]<arr[j+1])
-			{
-				cout<<"("<<arr[j+1]<<" > "<<arr[j]<<") No swap";
-			}
-			if(arr[j]>arr[j+1])
-			{
-				cout<<"("<<arr[j]<<" > "<<arr[j+1]<<") swap";
-				int temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-			
-							cout<<endl;
-		}
-		cout<<"\n+++++++++++++++++++++++++++++++++++++\n";
-	}
+	
+	bubble_sort b;
 	return 0;
 }
 
